d3907a63589387b0e974399a4d20857353f940ba
KirTheGreat <mr.mirsaid899@gmail.com>
2023-05-27 22:57:05 +1000

    first bot

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include <stdio.h>
+#include <tgbot/tgbot.h>
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    TgBot::Bot bot("TOKEN");
+
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message)
+                              { bot.getApi().sendMessage(message->chat->id, "Hi, this bot was made by Mirsaid!"); });
+
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message)
+                              { bot.getApi().sendMessage(message->chat->id, "Hi, this bot was made by Mirsaid!"); });
+
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message)
+                                 {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text); });
+    try
+    {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true)
+        {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    }
+    catch (TgBot::TgException &e)
+    {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
 }
